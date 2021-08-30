7eee8ff0e086eecc7af17a99356599aa5ccd71bd
Victor <arturopt25@gmail.com>
2021-08-30 18:43:43 -0400

    initial project

---
+++ epaycotest/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './Components/Form'
+import Info from './Components/Info'
+function App() {
+  return (
+    <div className="App">
+      <Form />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
